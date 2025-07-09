f1ee8999b20d498ddda65073f73b7a2f7986f545
FadelArdyatam <155771319+FadelArdyatam@users.noreply.github.com>
2025-07-09 22:32:59 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# contoh data
+books = [
+    {"id": 1, "title": "Python for Beginners", "author": "John Doe"},
+    {"id": 2, "title": "Flask API Guide", "author": "Jane Smith"}
+]
+
+# endpoint flask
+
+@app.route('/api/books', methods=['GET'])
+def getBooks():
+    return jsonify(books)
+
+@app.route('/api/books', methods=['POST'])
+def addBook():
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify({'message': 'new book added'}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
